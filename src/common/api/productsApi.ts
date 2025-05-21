f8ded6a668ecf626a88ec1aaed337892d5a667b6
bloodyrainday <bloodyrainday@gmail.com>
2025-05-21 12:03:43 +0300

    add productsApi

---
+++ src/common/api/productsApi.ts
@@ -0,0 +1,8 @@
+import type { ProductType } from "../../App";
+import { instance } from "../instance/instance";
+
+export const productsApi = {
+  getProducts() {
+    return instance.get<ProductType[]>("todo-lists");
+  },
+};
--- src/model/products-reducer.ts
+++ src/model/products-reducer.ts
@@ -1,25 +1,80 @@
-import { createAction } from "@reduxjs/toolkit";
 import type { ProductType } from "../App";
+import { productsApi } from "../common/api/productsApi";
+import { createAppSlice } from "../common/utils/createAppSlice";
 
-const initialState: ProductType[] = [];
+export const productsSlice = createAppSlice({
+  name: "products",
+  initialState: [] as ProductType[],
+  reducers: (create) => ({
+    //async actions
+    fetchProducts: create.asyncThunk(
+      async (_arg, { rejectWithValue, dispatch }) => {
+        try {
+          const res = await productsApi.getProducts();
 
-export const deleteProductAC = createAction<{ id: number }>(
-  "products/delete_product"
-);
+          return { todolists: res.data };
+        } catch (err: any) {
+          return rejectWithValue(null);
+        }
+      },
+      {
+        fulfilled: (_state, action) => {
+          // action.payload?.todolists.forEach((tl) => {
+          //   state.push({ ...tl, filter: "all", entityStatus: "idle" })
+          // })
 
-export type DeleteProductAction = ReturnType<typeof deleteProductAC>;
+          return action.payload?.todolists.map((tl) => {
+            return { ...tl, filter: "all", entityStatus: "idle" };
+          });
+        },
+      }
+    ),
+  }),
+  // extraReducers: (builder) => {
+  //   builder.addCase(clearDataAC, (_state, _action) => {
+  //     return [];
+  //   });
+  // },
 
-type Actions = DeleteProductAction;
+  selectors: {
+    selectProducts: (state) => state,
+  },
+});
 
-export const productsReducer = (
-  state: ProductType[] = initialState,
-  action: Actions
-): ProductType[] => {
-  switch (action.type) {
-    case "products/delete_product": {
-      return state.filter((product) => product.id !== action.payload.id);
-    }
-    default:
-      return state;
-  }
-};
+export const productsReducer = productsSlice.reducer;
+export const { fetchProducts } = productsSlice.actions;
+export const { selectProducts } = productsSlice.selectors;
+
+// import { createAction } from "@reduxjs/toolkit";
+// import type { ProductType } from "../App";
+
+// const initialState: ProductType[] = [];
+
+// export const deleteProductAC = createAction<{ id: number }>(
+//   "products/delete_product"
+// );
+
+// export const createProductAC = createAction<ProductType>(
+//   "products/create_product"
+// );
+
+// export type DeleteProductAction = ReturnType<typeof deleteProductAC>;
+// export type CreateProductAction = ReturnType<typeof createProductAC>;
+
+// type Actions = DeleteProductAction | CreateProductAction;
+
+// export const productsReducer = (
+//   state: ProductType[] = initialState,
+//   action: Actions
+// ): ProductType[] => {
+//   switch (action.type) {
+//     case "products/create_product": {
+//       return state.filter((product) => product.id !== action.payload.id);
+//     }
+//     case "products/delete_product": {
+//       return state.filter((product) => product.id !== action.payload.id);
+//     }
+//     default:
+//       return state;
+//   }
+// };
